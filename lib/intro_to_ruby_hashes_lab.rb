62e01b5b02fd7cf53e95aae767f45b4e22f9a58e
Maya Blevins <maya.blevins@gmail.com>
2020-05-04 21:11:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,16 @@ def new_hash
 end
 
 def my_hash
-  {:teach = 19}
+  {:teach => 19}
 end
 
 def pioneer
-  {:name = 'Grace Hopper'}
+  {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
   
-  {:id = number}
+  {:id => number}
 end
